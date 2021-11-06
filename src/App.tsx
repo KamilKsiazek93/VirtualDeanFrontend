2479bb177df12682bd3ab88e55d8413d08d6e7c6
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2021-11-06 16:20:28 +0100

    Setup for connecting with server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { webAPIUrl } from './AppSettings';
+
+const brotherUrl = webAPIUrl + `/brothers`;
+
+const fetchingData = () => {
+  fetch(`${webAPIUrl}/brothers`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+}
 
 function App() {
+  fetchingData()
   return (
     <div className="App">
       <header>
---
+++ src/AppSettings.ts
@@ -0,0 +1,3 @@
+export const server = 'https://localhost:44398';
+
+export const webAPIUrl = `${server}/api/offices`;
