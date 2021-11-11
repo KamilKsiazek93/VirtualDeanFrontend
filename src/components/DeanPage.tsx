4b10b478777e5fc4b37b24f9bc3892fbd9aa6422
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2021-11-11 17:56:09 +0100

    Added bootstrap lib

--- src/components/DeanPage.tsx
+++ src/components/DeanPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Link } from "react-router-dom";
 import { DeanOffice } from "./DeanOffice";
 import { KitchenOffice } from './KitchenOffice';
 
 export const DeanPage = () => (
     <div>
-        <p>DeanPage</p>
+        <h2>Panel dziekana</h2>
+        <Link className="nav-link" to="oficja">Wyznacz oficja tygodniowe</Link>
+        <Link to="oficja-kuchenne">Wyznacz oficja kuchenne</Link>
         <Routes>
             <Route path="oficja-kuchenne" element={<KitchenOffice />} />
             <Route path="oficja" element={<DeanOffice />} />
