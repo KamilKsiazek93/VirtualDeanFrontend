e3ac19ed6809762f47513eec00da75c8a0ab1375
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2021-11-12 15:54:17 +0100

    Setup proccess adding brother to db from form

--- src/components/DeanPage.tsx
+++ src/components/DeanPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { AddBrotherForm } from "./AddBrotherForm";
 import { DeanOffice } from "./DeanOffice";
 import { KitchenOffice } from './KitchenOffice';
 
@@ -7,10 +8,12 @@ export const DeanPage = () => (
     <div>
         <h2>Panel dziekana</h2>
         <Link className="nav-link" to="oficja">Wyznacz oficja tygodniowe</Link>
-        <Link to="oficja-kuchenne">Wyznacz oficja kuchenne</Link>
+        <Link className="nav-link" to="oficja-kuchenne">Wyznacz oficja kuchenne</Link>
+        <Link className="nav-link" to="dodaj-brata">Dodaj brata</Link>
         <Routes>
             <Route path="oficja-kuchenne" element={<KitchenOffice />} />
             <Route path="oficja" element={<DeanOffice />} />
+            <Route path="dodaj-brata" element={<AddBrotherForm />} />
         </Routes>
     </div>
 )
