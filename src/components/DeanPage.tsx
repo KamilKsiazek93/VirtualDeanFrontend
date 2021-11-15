af60443d687adf4a53d647a6fc9b0c836201ec9c
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2021-11-15 19:27:05 +0100

    Refactor code. Adding components for main menu and dean menu

--- src/components/DeanPage.tsx
+++ src/components/DeanPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AddBrotherForm } from "./AddBrotherForm";
+import { DeanNavbar } from "./DeanNavbar";
 import { DeanOffice } from "./DeanOffice";
 import { EditBrothers } from "./EditBrother";
 import { KitchenOffice } from './KitchenOffice';
@@ -10,22 +10,7 @@ import { ObstacleConst } from "./ObstacleConst";
 
 export const DeanPage = () => (
     <div>
-        <h2>Panel dziekana</h2>
-        <Navbar bg="light" expand="lg">
-            <Container>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to="oficja">Wyznacz oficja tygodniowe</Nav.Link>
-                    <Nav.Link as={Link} to="oficja-kuchenne">Wyznacz oficja kuchenne</Nav.Link>
-                    <Nav.Link as={Link} to="dodaj-brata">Dodaj brata</Nav.Link>
-                    <Nav.Link as={Link} to="edytuj-braci">Edytuj braci</Nav.Link>
-                    <Nav.Link as={Link} to="przeszkody-stale">Przedzkody stałe</Nav.Link>
-                    <Nav.Link as={Link} to="przeszkody-miedzy-oficjami">Przeszkody między oficjami</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <DeanNavbar />
 
         <Routes>
             <Route path="oficja-kuchenne" element={<KitchenOffice />} />
---
+++ src/components/MainNavbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export const MainNavbar = () => (
+    <Navbar bg="light" expand="lg">
+        <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="main-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link as={Link} to="dziekan">Strefa dziekana</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+)
