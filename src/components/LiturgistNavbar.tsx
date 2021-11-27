bc70a1d4aed219e425fd04a3711aa776595f5cc4
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2021-11-27 14:41:12 +0100

    Added skeletor for liturgist page components

--- src/components/LiturgistNavbar.tsx
+++ src/components/LiturgistNavbar.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const MainNavbar = () => (
+export const LiturgistNavbar = () => (
     <Navbar bg="light" expand="lg">
         <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="main-navbar-nav">
+            <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link as={Link} to="kantor">Strefa kantora</Nav.Link>
-                <Nav.Link as={Link} to="liturgista">Strefa liturgisty</Nav.Link>
-                <Nav.Link as={Link} to="dziekan">Strefa dziekana</Nav.Link>
+                <Nav.Link as={Link} to="taca">Wyznacz tacę</Nav.Link>
+                <Nav.Link as={Link} to="oficja">Wyznacz oficja liturgiczne</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
--- src/components/LiturgistPage.tsx
+++ src/components/LiturgistPage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { LiturgistNavbar } from "./LiturgistNavbar";
 
 export const LiturgistPage = () => (
-    <div>LiturgistPage</div>
+    <div>
+        <LiturgistNavbar />
+        <Routes>
+            <Route path="taca" />
+            <Route path="oficja" />
+        </Routes>
+    </div>
 )
