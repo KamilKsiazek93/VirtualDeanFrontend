acb358979d1eb84620638d38ac692d7068504406
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-03-24 20:24:01 +0100

    Prepare template for liturgist office page

---
+++ src/components/LiturgistOffice.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const LiturgistOffice = () => {
+    return (
+        <div>
+            Wyznaczanie oficjów liturgicznych
+            
+        </div>
+    )
+}
--- src/components/LiturgistPage.tsx
+++ src/components/LiturgistPage.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { AddTray } from "./AddTray";
 import { LiturgistNavbar } from "./LiturgistNavbar";
+import { LiturgistOffice } from "./LiturgistOffice";
 
 export const LiturgistPage = () => (
     <div>
         <LiturgistNavbar />
         <Routes>
             <Route path="taca" element={<AddTray />} />
-            <Route path="oficja" />
+            <Route path="oficja" element={<LiturgistOffice />} />
         </Routes>
     </div>
 )
