a49993938870a4da62c907a45d2ebd206cb1f1d0
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-04-23 11:37:18 +0200

    Add class for component in liturgist page

--- src/components/LiturgistOffice.tsx
+++ src/components/LiturgistOffice.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { FormCheck, Table } from "react-bootstrap";
+import { Button, FormCheck, Table } from "react-bootstrap";
 import { getBaseBrothersForLiturgistOffice } from "./ApiConnection";
 import { BaseBrother } from "./Brother";
 
@@ -18,44 +18,47 @@ export const LiturgistOffice = () => {
 
     return (
         <div>
-            Wyznaczanie oficjów liturgicznych
+            <h2 className="header-frame">Wyznaczanie oficjów liturgicznych</h2>
             <div className="message-body">{message}</div>
-            <Table striped bordered hover variant="light">
-                <thead>
-                    <tr>
-                        <th>Lp</th>
-                        <th>Imię</th>
-                        <th>Naziwsko</th>
-                        <th>MO</th>
-                        <th>MK</th>
-                        <th>MŚ</th>
-                        <th>KR</th>
-                        <th>Tur</th>
-                        <th>Suc1</th>
-                        <th>Suc2</th>
-                        <th>Resp1</th>
-                        <th>Resp2</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {brothers?.map((brother:BaseBrother, index) => 
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{brother.name}</td>
-                        <td>{brother.surname}</td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                        <td><FormCheck /> </td>
-                    </tr>
-                    )}
-                </tbody>
-            </Table>
+            <div className="table-center">
+                <Table striped bordered hover variant="light">
+                    <thead>
+                        <tr>
+                            <th>Lp</th>
+                            <th>Imię</th>
+                            <th>Naziwsko</th>
+                            <th>MO</th>
+                            <th>MK</th>
+                            <th>MŚ</th>
+                            <th>KR</th>
+                            <th>Tur</th>
+                            <th>Suc1</th>
+                            <th>Suc2</th>
+                            <th>Resp1</th>
+                            <th>Resp2</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {brothers?.map((brother:BaseBrother, index) => 
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{brother.name}</td>
+                            <td>{brother.surname}</td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                            <td><FormCheck /> </td>
+                        </tr>
+                        )}
+                    </tbody>
+                </Table>
+                <Button className="button-center" variant="success">Dodaj oficja</Button>
+            </div>
         </div>
     )
 }
