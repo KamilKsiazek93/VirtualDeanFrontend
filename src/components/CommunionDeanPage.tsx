30e06a5fc892da559e6aca9e7e821c23a421b76a
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-05-09 19:34:18 +0200

    Added base navbar for communion dean page

--- src/components/CommunionDeanPage.tsx
+++ src/components/CommunionDeanPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes } from "react-router-dom";
+import { CommunionNavbar } from "./CommunionNavbar";
 
 export const CommunionDeanPage = () => (
     <div>
+        <CommunionNavbar />
         <Routes>
-            
+
         </Routes>
     </div>
 )
---
+++ src/components/CommunionNavbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../actions/auth";
+
+export const CommunionNavbar = () => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const storage = window.localStorage
+
+    const handleLogout = () => {
+        dispatch(logout)
+        storage.removeItem('user')
+        navigate('/')
+    }
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Button onClick={handleLogout}>Wyloguj</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
