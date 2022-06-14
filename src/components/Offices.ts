717af7a0b6a3966268a91e6fa67697fb278deef2
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-06-14 18:22:06 +0200

    Refactor LiturgistOffice Page. Add method for getting office name from database

--- src/components/Offices.ts
+++ src/components/Offices.ts
@@ -47,6 +47,12 @@ export interface ILastTray {
     brothersTrays: Array<string>;
 }
 
+export interface IOfficeNames {
+    id: number;
+    officeName: string;
+    officeAdmin: string;
+}
+
 export const addScholaToDb = async(data:CantorOfficeResponse[], accessToken:string):Promise<MessageBody | undefined> => {
     const result = await http<MessageBody, CantorOfficeResponse[]>({
         path: '/office-singing',
@@ -174,4 +180,12 @@ export const isOfficeAbleToSet = async(pipelineName:string): Promise<Boolean> =>
     return false
 }
 
-
+export const getOfficeNames = async(pathName:string): Promise<IOfficeNames[] | null> => {
+    const result = await http<IOfficeNames[]>({
+        path: `/office-name/${pathName}`
+    })
+    if(result.ok && result.body) {
+        return result.body.map((name, index) => name)
+    }
+    return []
+}
