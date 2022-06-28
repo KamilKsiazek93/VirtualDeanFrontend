97efd409013ada3b14c788e0be0cec66a99f325e
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-06-28 20:01:07 +0200

    Added obstacle from brothers on cantor page

--- src/components/Schola.tsx
+++ src/components/Schola.tsx
@@ -4,10 +4,12 @@ import { addScholaToDb, CantorOfficeResponse, isOfficeAbleToSet } from "./Office
 import { FormCheck, Table, Button } from 'react-bootstrap';
 import { getBaseBrothersForSchola } from "./ApiConnection";
 import { MessageIfOfficeIsAlreadySet } from "./MessageIfOfficeIsAlreadySet";
+import { getObstacleFromBrothers, IObstacleFromBrothers } from "./Obstacle";
 
 export const Schola = () => {
 
     const [brothers, setBrothers] = useState<Array<SingingBrothers> | null>(null);
+    const [obstaclesFromBrothers, setObstaclesFromBrothers] = useState<Array<IObstacleFromBrothers> | null>()
     const [message, setMessage] = useState<string>()
     const [isScholaAbleToSet, setInfoAboutOfficeSet] = useState<Boolean>()
 
@@ -19,20 +21,48 @@ export const Schola = () => {
         const getSingingBrothersFromDb = async() => {
             const brothers = await getBaseBrothersForSchola()
             setBrothers(brothers);
+            const obstaclesFromBrothers = await getObstacleFromBrothers()
+            setObstaclesFromBrothers(obstaclesFromBrothers)
             const isScholaAbleToSet = await isOfficeAbleToSet('/pipeline-status/CANTOR')
             setInfoAboutOfficeSet(isScholaAbleToSet)
         }
         getSingingBrothersFromDb();
     }, []);
 
-    const handleOffice = (brotherId:number, cantorOffice:string) => {
+    const setCheckboxValue = (id:string, value:boolean):void => {
+        let checkedBox = document.getElementById(id) as HTMLInputElement
+        checkedBox.checked = value
+    }
+    const isAvailableCheck = (brotherId:number, officeName:string):Boolean => {
+        const isInBrotherObstacleList = obstaclesFromBrothers?.find(item => item.brotherId === brotherId && 
+            item.obstacles.find(o => o.includes(officeName)))
+        if(isInBrotherObstacleList) {
+            setMessage("Brat zgłosił przeszkodę na to oficjum")
+            return false
+        }
+        return true
+    }
+
+    const pushObjectToArrayTray = (brotherId:number, cantorOffice:string):Boolean => {
         const objectExist = offices.find(office => office.brotherId === brotherId && office.cantorOffice === cantorOffice);
         if(!objectExist?.brotherId) {
             offices.push({brotherId, weekOfOffices: 0, cantorOffice})
+            return true
         } else {
             offices = offices.filter(office => office !== objectExist)
+            return false
         }
     }
+
+    const handleOffice = (brotherId:number, cantorOffice:string, index:number) => {
+        const id = index.toString() + cantorOffice;
+        if(isAvailableCheck(brotherId, cantorOffice) && pushObjectToArrayTray(brotherId, cantorOffice)) {
+            setCheckboxValue(id, true)
+        } else {
+            setCheckboxValue(id, false)
+        }
+    }
+
 
     const handleSendCantorOffice = async() => {
         const result = await addScholaToDb(offices, jwtToken);
@@ -61,8 +91,8 @@ export const Schola = () => {
                                 <td>{index+1}</td>
                                 <td>{brother.name}</td>
                                 <td>{brother.surname}</td>
-                                <td><FormCheck onChange={() => handleOffice(brother.id, "S")} /></td>
-                                <td><FormCheck onChange={() => handleOffice(brother.id, "PS")} /></td>
+                                <td><FormCheck id={index.toString() + "S"} onChange={() => handleOffice(brother.id, "S", index)} /></td>
+                                <td><FormCheck id={index.toString() + "PS"} onChange={() => handleOffice(brother.id, "PS", index)} /></td>
                             </tr>
                         )}
                     </tbody>
