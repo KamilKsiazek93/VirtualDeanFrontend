a7e0502b42177f3358d083089c33c7148a0a5b52
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2022-09-01 21:01:38 +0200

    Create file for ChangePassword components. Import it into Brother Page

--- src/components/Brother/BrotherNavbar.tsx
+++ src/components/Brother/BrotherNavbar.tsx
@@ -23,6 +23,7 @@ export const BrotherNavbar = () => {
                 <Nav className="me-auto">
                     <Nav.Link as={Link} to="dashboard">Dashboard</Nav.Link>
                     <Nav.Link as={Link} to="przeszkody">Zgłoś przeszkody</Nav.Link>
+                    <Nav.Link as={Link} to="zmiana-hasla">Zmień hasło</Nav.Link>
                     <Button onClick={handleLogout}>Wyloguj</Button>
                 </Nav>
                 </Navbar.Collapse>
---
+++ src/components/ChangePassword.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ChangePassword = () => {
+    return (
+        <div>
+            <h2 className="header-frame">Zmiana hasła</h2>
+        </div>
+    )
+}
